96c48b764107fbdaf7c986c6324a080bad87e748
Prakash4844 <pk484442@gmail.com>
2023-06-18 22:18:24 +0530

    Optimized Code

--- main.py
+++ main.py
@@ -16,27 +16,40 @@ author_name = 'Prakash4844'
 # Get today's date
 today = datetime.date.today().isoformat()
 
-# Get list of issues with label 'Request'
-issues_url = f'https://api.github.com/repos/{author_name}/{repository_name}/issues?labels=Request&state=open'
+# Issues API URL
+issues_url = f'https://api.github.com/repos/{author_name}/{repository_name}/issues'
 
-issue_response = requests.get(issues_url)
-issues = issue_response.json()
+# Issue Parameters
+label_name = "Request"
+issue_state = "open"
 
-for issue in issues:
-    issue_username = issue['user']['login']
-    issue_username_avatar_url = issue['user']['avatar_url']
-    issue_id = issue['id']
-    issue_body = issue['body']
+# Parameters for filtering issues by label
+params = {
+    "labels": label_name,
+    "state": issue_state,
+}
 
-    # Find the position of the colon (:) to separate the label and the value
-    name_start = issue_body.find(':') + 2
-    email_start = issue_body.find('Email:') + 7
+issue_response = requests.get(issues_url, params=params)
 
-    # Extract the name and email using string slicing
-    name = issue_body[name_start:issue_body.find('\r')]
-    email = issue_body[email_start:]
+if issue_response.status_code == 200:
+    issues = issue_response.json()
 
-    print("Name:", name)
-    print("Email:", email)
+    for issue in issues:
+        issue_username = issue['user']['login']
+        issue_username_avatar_url = issue['user']['avatar_url']
+        issue_id = issue['id']
+        issue_body = issue['body']
 
-print('test')
+        # Find the position of the colon (:) to separate the label and the value
+        name_start = issue_body.find(':') + 2
+        email_start = issue_body.find('Email:') + 7
+
+        # Extract the name and email using string slicing
+        name = issue_body[name_start:issue_body.find('\r')]
+        email = issue_body[email_start:]
+
+        print("Name:", name)
+        print("Email:", email)
+
+else:
+    print("Failed to fetch issues. Status code:", issue_response.status_code)
